4912e8e0f2bcc37458ad23630180b6342746c82a
yadhumanikandan <yadhumanikandan0@gmail.com>
2023-04-30 21:40:32 +0530

    subprocess test

--- monitorpy.py
+++ monitorpy.py
@@ -1,7 +1,17 @@
 import subprocess
 
-cmd = ['']
 
-subprocess.run(cmd)
+def enable():
+    subprocess.run("ifconfig wlan0 down", shell=True)
+    subprocess.run("ifconfig wlan0 mode monitor", shell=True)
+    subprocess.run("ifconfig wlan0 up", shell=True)
 
 
+def disable():
+    subprocess.run("airmon-ng stop wlan0", shell=True)
+    subprocess.run("ifconfig wlan0 up", shell=True)
+    subprocess.run("service NetworkManager start", shell=True)
+
+
+
+enable()
