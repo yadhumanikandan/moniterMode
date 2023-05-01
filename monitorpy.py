29929c6fa20165450225ad48923ab92dea3245b3
yadhumanikandan <yadhumanikandan0@gmail.com>
2023-05-01 16:41:56 +0530

    ifconfig to iwconfig

--- monitorpy.py
+++ monitorpy.py
@@ -3,7 +3,7 @@ import subprocess
 
 def enable():
     subprocess.run("ifconfig wlan0 down", shell=True)
-    subprocess.run("ifconfig wlan0 mode monitor", shell=True)
+    subprocess.run("iwconfig wlan0 mode monitor", shell=True)
     subprocess.run("ifconfig wlan0 up", shell=True)
 
 
